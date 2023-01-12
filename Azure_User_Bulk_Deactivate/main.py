76731cb03f6c9db2add6b664ce74ffa264b87892
Hideki Inoue <hideki.i@protonmail.com>
2023-01-12 17:57:37 -0300

    Added Azure Bulk Operations (Delete, Deactivate, Group Insert)

---
+++ Azure_User_Bulk_Deactivate/main.py
@@ -0,0 +1,37 @@
+from azure.cli.core import get_default_cli
+from time import strftime, time
+
+FILENAME_USERS = "Azure_User_Bulk_Deactivate/deactivate_list.txt"
+
+def az_cli(args_rec: str):
+    args = (args_rec + " --output none").split()
+    cli = get_default_cli()
+    cli.invoke(args)
+    if cli.result.result:
+        return cli.result.result
+    elif cli.result.error:
+        raise cli.result.error
+    return True
+
+def main():
+    '''
+        Pegar a lista de vnets e subnets em dict
+        mapear as vnets e subnets da vm pelo dict
+    '''
+
+    # Getting Group List
+    print(f"Reading all users from {FILENAME_USERS}")
+    st = time()
+
+    with open(FILENAME_USERS, mode="r", encoding="utf8") as remove_file:
+        for line_id in remove_file.readlines():
+            _user_id = str(line_id.split()[0])
+            user_info = az_cli(f"ad user show --id {_user_id}")
+            print(f"Disabling ID: {_user_id} - {user_info['displayName']}... ", end="")
+            res = az_cli(f"ad user update --id {_user_id} --account-enabled false")
+            print(res, flush=True)
+
+    print(f"Done in: {time() - st} seconds")
+
+if __name__ == "__main__":
+    main()
